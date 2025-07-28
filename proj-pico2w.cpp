415e04b4f8c7011de53653cd893cb8d65e09621d
main_user <k05075600@gmail.com>
2025-07-28 13:11:19 +0000

    main

--- proj-pico2w.cpp
+++ proj-pico2w.cpp
@@ -1,93 +1,56 @@
 #include <cmath> 
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
-
+/*
+#include "ssd1306.h"
+#include "image.h"
+*/
 #ifdef CYW43_WL_GPIO_LED_PIN
 #include "pico/cyw43_arch.h"
 #endif
 
-#define BUTTON_PIN_1 2
-#define BUTTON_PIN_2 3
-#define BUTTON_PIN_3 4
-
-
-#ifndef LED_DELAY_MS
-#define LED_DELAY_MS 500
-#endif
-
+#define BUTTON_PIN_RIGHT 2
+#define BUTTON_PIN_LEFT 6
+#define BUTTON_PIN_UP 4
+#define BUTTON_PIN_DOWN 5
+#define BUTTON_PIN_CENTER 7
 
 #define I2C_PORT i2c0
 #define I2C_SDA 16
 #define I2C_SCL 17
 
-
-int pico_led_init(void) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    return cyw43_arch_init();
-#endif
-}
-
-void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-#endif
-}
-
-
-
 int main()
 {
     stdio_init_all();
 
-    i2c_init(I2C_PORT, 400*1000); // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400*1000);
 
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
 
-    gpio_init(BUTTON_PIN_1);
-    gpio_set_dir(BUTTON_PIN_1, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN_1);
+    gpio_init(BUTTON_PIN_RIGHT);
+    gpio_set_dir(BUTTON_PIN_RIGHT, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_RIGHT);
 
-    gpio_init(BUTTON_PIN_2);
-    gpio_set_dir(BUTTON_PIN_2, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN_2);
+    gpio_init(BUTTON_PIN_LEFT);
+    gpio_set_dir(BUTTON_PIN_LEFT, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_LEFT);
 
-    gpio_init(BUTTON_PIN_3);
-    gpio_set_dir(BUTTON_PIN_3, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN_3);
+    gpio_init(BUTTON_PIN_UP);
+    gpio_set_dir(BUTTON_PIN_UP, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_UP);
 
-        int rc = pico_led_init(); 
-    if (rc != PICO_OK){
-        printf("Initialisation failed");
-        fflush(stdout);
-    }   
+    gpio_init(BUTTON_PIN_DOWN);
+    gpio_set_dir(BUTTON_PIN_DOWN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_DOWN);
 
-     while(true){
-        if(!gpio_get(BUTTON_PIN_1)){
-            pico_set_led(true);
-            sleep_ms(LED_DELAY_MS);
-            pico_set_led(false);
-        }
-        if(!gpio_get(BUTTON_PIN_2)){
-            pico_set_led(true);
-            sleep_ms(LED_DELAY_MS);
-            pico_set_led(false);
-        }
-        if(!gpio_get(BUTTON_PIN_3)){
-            pico_set_led(true);
-            sleep_ms(LED_DELAY_MS);
-            pico_set_led(false);
-        }
-    }
-     
+    gpio_init(BUTTON_PIN_CENTER);
+    gpio_set_dir(BUTTON_PIN_CENTER, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_CENTER);
 }
